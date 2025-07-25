4bc91d54b908621de350e0866a730b73388bb7e3
jhparkland <pwjdgus1017@donga.ac.kr>
2025-07-25 09:29:42 +0000

    upload

---
+++ 8-3.py
@@ -0,0 +1,70 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.datasets as ds
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dropout,Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    print("GPU 사용 가능:", gpus)
+else:
+    print("GPU를 사용할 수 없습니다. CPU로 실행됩니다.")
+
+(x_train,y_train),(x_test,y_test)=ds.mnist.load_data()
+x_train=x_train.reshape(60000,28,28,1)
+x_test=x_test.reshape(10000,28,28,1)
+x_train=x_train.astype(np.float32)/255.0
+x_test=x_test.astype(np.float32)/255.0
+y_train=tf.keras.utils.to_categorical(y_train,10)
+y_test=tf.keras.utils.to_categorical(y_test,10)
+
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1
+)
+datagen.fit(x_train)
+
+cnn=Sequential()
+cnn.add(Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)))
+cnn.add(Conv2D(32,(3,3),activation='relu'))
+cnn.add(MaxPooling2D(pool_size=(2,2)))
+cnn.add(Dropout(0.25))
+cnn.add(Conv2D(64,(3,3),activation='relu'))
+cnn.add(Conv2D(64,(3,3),activation='relu'))
+cnn.add(MaxPooling2D(pool_size=(2,2)))
+cnn.add(Dropout(0.25))
+cnn.add(Flatten())
+cnn.add(Dense(units=512,activation='relu'))
+cnn.add(Dropout(0.5))
+cnn.add(Dense(units=10,activation='softmax'))
+
+cnn.compile(loss='categorical_crossentropy',optimizer=Adam(learning_rate=0.001),metrics=['accuracy'])
+hist=cnn.fit(datagen.flow(x_train, y_train, batch_size=64), epochs=50, validation_data=(x_test, y_test), verbose=2)
+
+cnn.save('./cnn_v2.h5')
+
+res=cnn.evaluate(x_test,y_test,verbose=0) 
+print('정확률=',res[1]*100)
+
+import matplotlib.pyplot as plt
+
+plt.plot(hist.history['accuracy'])
+plt.title('Accuracy graph')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train','Validation'])
+plt.grid()
+plt.savefig('8-3-accuracy.png')
+
+plt.plot(hist.history['loss'])
+plt.title('Loss graph')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train','Validation'])
+plt.grid()
+plt.savefig('8-3-loss.png')
